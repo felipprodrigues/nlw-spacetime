d5e4da5a4d26000c99fbd4210d4abbd64ca2dc49
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-05-20 19:52:51 -0300

    push several new routing features

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
+import { useRouter } from 'expo-router'
 
 import {
   useFonts,
@@ -12,15 +15,61 @@ import blurBg from 'mobile/assets/bg-blur.png'
 import Stripes from 'mobile/assets/stripes.svg'
 import NLWLogo from 'mobile/assets/nwl-logo.svg'
 import { styled } from 'nativewind'
+import { useEffect } from 'react'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/96b8c59b63a0d0a349b4',
+}
+
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: '96b8c59b63a0d0a349b4',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', { code })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
 
   // mostra a interface somente depois de carregar as fontes
   if (!hasLoadedFonts) {
@@ -52,7 +101,10 @@ export default function App() {
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
         >
-          <Text className="font-alt text-sm uppercase text-black">
+          <Text
+            className="font-alt text-sm uppercase text-black"
+            onPress={() => promptAsync()}
+          >
             Cadastrar lembrança
           </Text>
         </TouchableOpacity>
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,28 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // todas URLs de front-end poderão acessar nosso back-end
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('http server running on http://localhost:3333')
