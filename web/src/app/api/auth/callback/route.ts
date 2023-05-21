7c5d26cff4c9c540449db5c0c62ed53b4706a157
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-05-21 17:03:29 -0300

    add middleware and new routes and pages

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -10,9 +10,11 @@ export async function GET(request: NextRequest) {
     code,
   })
 
+  const redirectTo = request.cookies.get('redirectTo')?.value
+
   const { token } = registerResponse.data
 
-  const redirectUrl = new URL('/', request.url)
+  const redirectUrl = redirectTo ?? new URL('/', request.url)
 
   const cookieExpiresInSeconds = 60 * 60 * 24 * 30
 
